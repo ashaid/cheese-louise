1110e9410bc47658b737a82f0a7f12ae18f3da7e
tony <ashaid2@lsu.edu>
2023-07-13 00:47:38 -0500

    added post request /deployCheese

--- api/createServer.js
+++ api/createServer.js
@@ -1,14 +1,24 @@
 const express = require("express");
+const { getChannels } = require("./getChannels");
 
 const createServer = (client) => {
   const app = express();
   const port = 3000;
 
-  app.get("/", (_, res) => {
-    res.send(`${client.user.username} says Hello World!`);
+  app.use(express.json());
+  app.listen(port, () => {
+    console.log(`Express is listening on ${port}`);
   });
 
-  return app;
+  app.post("/deployCheese", (req, res) => {
+    const channels = getChannels();
+    for (let key in channels) {
+      console.log(key);
+      const channelToSendMessage = client.channels.cache.get(key);
+      channelToSendMessage.send(req.body.cheese);
+    }
+    res.status(200).send("POST Recieved");
+  });
 };
 
 module.exports = { createServer };
