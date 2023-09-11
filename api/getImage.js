5a949eaa07a1e62ff9acd21ecb726fcffbe2aa7c
ashaid <ashaid2@lsu.edu>
2023-09-11 01:26:49 -0500

    fixed retry function in get image

--- api/getImage.js
+++ api/getImage.js
@@ -1,48 +1,42 @@
 const { bearerKey } = require("../config.json");
 
-async function retryFetch(url, maxRetries, delay) {
-  for (let retry = 0; retry <= maxRetries; retry++) {
+async function queryImage(data, maxRetries = 5, retryDelay = 10000) {
+  let retries = 0;
+
+  while (retries < maxRetries) {
     try {
-      const response = await fetch(url);
+      const response = await fetch(
+        "https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5",
+        {
+          headers: {
+            Authorization: bearerKey,
+          },
+          method: "POST",
+          body: JSON.stringify(data),
+        }
+      );
+
       if (response.ok) {
-        return response;
+        const blob = await response.blob();
+        const stream = await blob.arrayBuffer();
+        return stream;
+      } else {
+        // Handle non-ok response here, you can throw an error or log it.
+        console.error(`Request failed with status: ${response.status}`);
       }
     } catch (error) {
-      console.error(`Error on attempt ${retry + 1}: ${error}`);
+      // Handle fetch errors here, you can throw an error or log it.
+      console.error("Fetch error:", error);
     }
 
-    if (retry < maxRetries) {
-      console.log(`Retrying in ${delay} milliseconds...`);
-      await new Promise(resolve => setTimeout(resolve, delay));
-    }
-  }
-
-  throw new Error(`Failed to fetch after ${maxRetries} retries.`);
-}
-
-async function queryImageWithRetry(data, maxRetries, delay) {
-  const url = "https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5"; 
-  const headers = {
-    Authorization: bearerKey,
-  };
-  
-  for (let retry = 0; retry <= maxRetries; retry++) {
-    try {
-      const response = await retryFetch(url, 1, delay, {
-        headers,
-        method: "POST",
-        body: JSON.stringify(data),
-      });
-
-      const blob = await response.blob();
-      const stream = await blob.arrayBuffer();
-      return stream;
-    } catch (error) {
-      console.error(`Error on attempt ${retry + 1}: ${error}`);
+    // Increment retries and wait before the next retry
+    retries++;
+    if (retries < maxRetries) {
+      await new Promise((resolve) => setTimeout(resolve, retryDelay));
     }
   }
 
-  throw new Error(`Failed to query image after ${maxRetries} retries.`);
+  throw new Error(`Failed after ${maxRetries} retries`);
 }
 
-module.exports = { queryImageWithRetry };
+module.exports = { queryImage };
